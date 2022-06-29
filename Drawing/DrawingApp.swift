0a13c65fe31c8fb28d314c188cab56242d8e0a9b
Sadman Adib <sadmanadib67@gmail.com>
2022-06-29 17:15:32 +0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Sadman Adib on 29/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
